bf02eafee99f15f02dddf68461008dde056c079a
Navdeshsinghsaini <naveshsinghsaini@gmail.com>
2023-03-20 13:24:03 +0530

    second1 commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const out = document.getElementById('output');
+
+const hello =  new Promise ((resolve , reject ) => {
+        setTimeout(() => {
+                 resolve ("Hello, world!")
+        },1000);
+    });
+
+// console.log (hello);
+hello.then((data) => {
+    out.innerHTML = data
+    console.log(data);
+})
+
+// myPromise.then((data) => {
+//     console.log(data);
+// })
